6015d513ac00ec59e21e6cfc87072686db918cdf
AE99328 <davide.belvedere@gmail.com>
2019-02-22 12:30:01 +0100

    Configured resteasy

---
+++ src/main/java/restServicesConf/RestEasyServices.java
@@ -0,0 +1,21 @@
+package restServicesConf;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+@ApplicationPath("/services")
+public class RestEasyServices extends Application {
+    private Set<Object> singletons = new HashSet<Object>();
+
+    //aggiungere tutte le classi che espongono servizi al singleton
+    public RestEasyServices() {
+
+    }
+
+    @Override
+    public Set<Object> getSingletons() {
+        return singletons;
+    }
+}
